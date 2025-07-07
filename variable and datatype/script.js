fe96f9dc41907b823996b90b46327f0047c3f6ce
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2025-07-07 21:14:10 +0600

    Add files via upload

---
+++ variable and datatype/script.js
@@ -0,0 +1,11 @@
+const Profile = {
+    username: "john_doe",
+    folling: 150,
+    followers: 200,
+    email: "john@example.com",
+    age: 30,
+    isActive: true,
+};
+
+console.log(Profile);
+
